7513c4eb11c2231f72250bf9c0ff69325246c006
ivan <rubberman88@rabler.ru>
2017-10-08 16:24:50 +0500

    add dop

--- Program.cs
+++ Program.cs
@@ -10,8 +10,7 @@ namespace Test
 			var machineName = Environment.MachineName;
 			var user = Environment.UserName;
 			var assemblyPath = Assembly.GetExecutingAssembly().Location;
-			Console.Write($"Tested by {user}@{machineName} at {assemblyPath}");
-			Console.Write("add");
+			Console.Write($"ADD Tested by {user}@{machineName} at {assemblyPath}");
 		}
 	}
 }
